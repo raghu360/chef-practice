0a48f6c922aa2df755874ab1c20d9c2a001975c0
raghu11 <raaaaghav.420@gmail.com>
2017-07-08 17:40:05 -0400

    add chef-repo

---
+++ .chef/knife.rb
@@ -0,0 +1,10 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "raghu11"
+client_key               "#{current_dir}/raghu11.pem"
+chef_server_url          "https://chefserver.mydomain.local/organizations/cloud"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
